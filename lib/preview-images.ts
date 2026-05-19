02063559dd521788cac36a7291dd7f208e6affd2
AntiEntropy-bit <AntiEntropy@pku.edu.cn>
2026-05-19 19:48:01 +0800

    Fix preview image lint error

--- lib/preview-images.ts
+++ lib/preview-images.ts
@@ -3,5 +3,6 @@ import { ExtendedRecordMap, PreviewImageMap } from 'notion-types'
 export async function getPreviewImageMap(
   _recordMap: ExtendedRecordMap
 ): Promise<PreviewImageMap> {
+  void _recordMap
   return {}
 }
